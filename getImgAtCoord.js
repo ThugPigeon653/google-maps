7ea24505108808b2e63322c41911c23d2e710be0
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-06-15 13:11:41 +1000

    Update getImgAtCoord.js

--- getImgAtCoord.js
+++ getImgAtCoord.js
@@ -1,1 +1,18 @@
-
+// requires script to feed in coord in matching format
+var searchQuery = "41°24'12.2\"N 2°10'2\"E";
+var searchInput = document.getElementById("searchboxinput");
+searchInput.value = searchQuery;
+var searchButton = document.querySelector('[aria-label="Search"]');
+searchButton.click();
+setTimeout(function() {
+  var thumbnailImage = document.querySelector('img[decoding="async"]');
+  var imageUrl = thumbnailImage.getAttribute("src");
+  fetch(imageUrl)
+    .then(response => response.blob())
+    .then(blob => {
+      var downloadLink = document.createElement("a");
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.setAttribute("download", searchQuery+".jpg");
+      downloadLink.click();
+    });
+}, 1000);
