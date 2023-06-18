8d680c76d6d83087b9000efc0ea5b128aaf82be5
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-06-18 17:27:31 +1000

    Create rotateImgView.js

---
+++ rotateImgView.js
@@ -0,0 +1,7 @@
+// rotate clockwise
+const clockwiseButton = document.querySelector('.sO0oCd');
+clockwiseButton.click();
+
+// rotate anticlockwise
+const anticlockwiseButton = document.querySelector('.VhYNTe');
+anticlockwiseButton.click();
